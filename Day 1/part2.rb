3e6e98e5c50783bae55efde31c6244ddb3898f18
TheWrongAlice <alice.cyan.carlsson@gmail.com>
2018-12-02 23:49:27 +0100

    Day 1

---
+++ Day 1/part2.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+# --- Part Two ---
+# You notice that the device repeats the same frequency change list over and over. To calibrate the device, you need to find the first frequency it reaches twice.
+#
+# For example, using the same list of changes above, the device would loop as follows:
+#
+# Current frequency  0, change of +1; resulting frequency  1.
+# Current frequency  1, change of -2; resulting frequency -1.
+# Current frequency -1, change of +3; resulting frequency  2.
+# Current frequency  2, change of +1; resulting frequency  3.
+# (At this point, the device continues from the start of the list.)
+# Current frequency  3, change of +1; resulting frequency  4.
+# Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+# In this example, the first frequency reached twice is 2. Note that your device might need to repeat its list of frequency changes many times before a duplicate frequency is found, and that duplicates might be found while in the middle of processing the list.
+#
+# Here are other examples:
+#
+# +1, -1 first reaches 0 twice.
+# +3, +3, +4, -2, -4 first reaches 10 twice.
+# -6, +3, +8, +5, -6 first reaches 5 twice.
+# +7, +7, -2, -7, -4 first reaches 14 twice.
+# What is the first frequency your device reaches twice?
+
+frequencies = []
+current_frequency = 0
+
+loop do
+  File.open('input.txt').each do |drift|
+    frequencies << current_frequency
+
+    operand = drift[0, 1]
+    value = drift[1, drift.length].to_i
+    if operand == '+'
+      current_frequency += value
+    else
+      current_frequency -= value
+    end
+
+    puts drift
+    puts current_frequency
+    if frequencies.include? current_frequency
+      puts current_frequency
+      exit
+    end
+
+  end
+end
